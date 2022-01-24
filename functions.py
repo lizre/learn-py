e8e2840daaa4ee45b62ab0563c7259ff3ee5d95d
Liz <lizre@github.com>
2022-01-24 11:44:39 -0500

    more

---
+++ functions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/eng python 3
+# starting with this ^ "shebang" statement means you don't have to specify python3 when you run
+
+def add_two(number):
+    """one-line summary: add 2 to any number!
+
+    further elaboration would go here
+    this is how addition works
+    """
+    
+    return number+2
--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 #!/usr/bin/eng python 3
 # starting with this ^ "shebang" statement means you don't have to specify python3 when you run
+
 x = 5
 print(x+  10)
+
