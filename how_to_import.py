bb1f58d9e96be489b675572518d03725d18c0945
Liz <lizre@github.com>
2022-02-03 09:50:03 -0500

    first

---
+++ how_to_import.py
@@ -0,0 +1,11 @@
+# import one function:
+from functions import add_two
+
+# Import whole module/script
+import functions as myfxns
+#then preface every function:
+#myfxns.add_two()
+
+from functions import *
+#bad practice; obscures where functions come from
+
